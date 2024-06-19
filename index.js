08bcb35261b3c70b68a2c1a10ca5449c18a0f83f
dm1886 <damianomiazzi@gmail.com>
2024-06-18 23:16:48 -0700

    Initial Commit Server

---
+++ index.js
@@ -0,0 +1,12 @@
+import express from 'express';
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+    res.send("<h1>Hello World! Im ONLINE</h1>" );
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});

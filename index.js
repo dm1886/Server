50b2e5514542f83c2aba2e0a11a8bf739acc1326
Damiano Miazzi <damianomiazzi@gmail.com>
2024-06-21 10:03:24 +0800

    Initial SetUp to Use and Build on Mac Docker

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import express from 'express';
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3002;
 
 app.get('/', (req, res) => {
     res.send("<h1>Hello World! Im ONLINE with dcker</h1>" );

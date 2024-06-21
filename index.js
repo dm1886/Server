ae3617fd8558775bf4104d5bc514b90c32e48b26
Damiano Miazzi <damianomiazzi@gmail.com>
2024-06-21 12:13:52 +0800

    fix the issue with the get

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const port = process.env.PORT || 3002;
 
 app.get('/', (req, res) => {
     res.send("<h1>Hello World! Im ONLINE with new test mac dcker</h1>" );
-    res.end(`Hello this is ${process.arch}` );
+    
 });
 
 app.listen(port, () => {

ee57c0e1e7969218640078c3163ea7dac3f834a3
dm1886 <damianomiazzi@gmail.com>
2024-06-26 00:07:09 -0700

    Update change from mac

--- index.js
+++ index.js
@@ -1,23 +1,11 @@
 import express from 'express';
-import bodyParser from 'body-parser';
-import bcryps from 'bcrypt';
-import jwt from 'jsonwebtoken';
-import pg from 'pg';
-// Test
-
 
 const app = express();
 const port = process.env.PORT || 3002;
-const secretKey = 'your_jwt_secret_key'; // Change this to a secure key
-
-app.use(bodyParser.json());
-
-// PostgreSQL connection
-
 
 app.get('/', (req, res) => {
     res.send("<h1>Hello World! Im ONLINE with new test mac dcker</h1>" );
-    
+  
 });
 
 app.listen(port, () => {
